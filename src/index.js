b0d9968b0abb2899532f75ffeeefe85bb989f2d5
baqer <baqer@haneens-MacBook-Pro.local>
2026-09-20 18:50:29 -0400

    changes for amplify

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 function HelloWorld(){
-  return <h1 className="greeting">Hello, World!</h1>;
+  return <h1 className="greeting">Hello, from Amplify!</h1>;
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
